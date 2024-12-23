13a18a938076a126d4b64f6157971c07ac55b36b
Anya ฅ^•ﻌ•^ฅ <147582797+ne4ertik@users.noreply.github.com>
2024-12-23 10:52:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalnyFile = @"C:\Users\lazur\OneDrive\Документы\код.txt"; 
+            string copyFile = @"C:\My works for college"; 
+            bool a = true; //перезапись
+
+            try
+            {
+                File.Copy(originalnyFile, copyFile, a);
+                Console.WriteLine("Файл успешно скопирован.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Ошибка при копировании файла: " + ex.Message);
+            }
+            
+        }
+    }
+}
