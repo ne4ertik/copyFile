183143df50aa209c0a1a8ef4a02e62eb403fbda3
Anya ฅ^•ﻌ•^ฅ <147582797+ne4ertik@users.noreply.github.com>
2024-12-23 10:56:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,30 @@
     {
         static void Main(string[] args)
         {
-            string originalnyFile = @"C:\Users\lazur\OneDrive\Документы\код.txt"; 
+            string originalnyFile1 = @"C:\Users\lazur\OneDrive\Документы\код.txt";
+            string originalnyFile2 = @"C:\Users\lazur\OneDrive\Документы\ABC.txt";
             string copyFile = @"C:\My works for college"; 
             bool a = true; //перезапись
 
             try
             {
-                File.Copy(originalnyFile, copyFile, a);
+                File.Copy(originalnyFile1, copyFile, a);
                 Console.WriteLine("Файл успешно скопирован.");
             }
             catch (IOException ex)
             {
                 Console.WriteLine("Ошибка при копировании файла: " + ex.Message);
             }
-            
+
+            try
+            {
+                File.Copy(originalnyFile2, copyFile, a);
+                Console.WriteLine("Файл успешно скопирован.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Ошибка при копировании файла: " + ex.Message);
+            }
         }
     }
 }
